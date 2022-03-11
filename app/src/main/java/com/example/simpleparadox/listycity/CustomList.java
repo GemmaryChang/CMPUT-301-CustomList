89d63984781907ed2edf20b323ed5cef3d836ba1
GemmaryChang <gemmary@ualberta.ca>
2022-03-11 08:44:51 -0700

    test case deleteCityTest

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -71,7 +71,9 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void delete(City city){
-        return;
+        if(cities.contains(city)){
+            cities.remove(city);
+        }
     }
 
 
